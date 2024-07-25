55cd46bc9f1bfc18c980f25eeefc9cc3419ec5c6
senepote <senepote@gmail.com>
2024-07-25 10:26:16 -0500

    added leadr slides

--- js/main.js
+++ js/main.js
@@ -70,34 +70,20 @@ $(() => {
   
 })
  
-// let slideIndex = 1;
-// showSlides(slideIndex);function plusSlide(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function showSlides(n) {
-//     let i;
-//     const slides = document.getElementsByClassName("slide");
-//     const designSlides = document.getElementsByClassName("design-slide");
-//     if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//     }
-//     slides[slideIndex-1].style.display = "block";
-
-//     if (n > designSlides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = designSlides.length}
-//     for (i = 0; i < designSlides.length; i++) {
-//       designSlides[i].style.display = "none";
-//     }
-//     designSlides[slideIndex-1].style.display = "block";
-//   }
-  
-//   function prevSlide() {
-//     plusSlide(-1);
-//   }
-  
-//   function nextSlide() {
-//     plusSlide(1);
-//   }
+let slideIndex = 1;
+showSlides(slideIndex);function plusSlide(n) {
+  showSlides(slideIndex += n);
+}function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}function prevSlide() {
+  plusSlide(-1);
+}function nextSlide() {
+  plusSlide(1);
+}
