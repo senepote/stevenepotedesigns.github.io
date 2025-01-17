68637dee7ff680c5ee2b69bdae3c7c804063bc57
senepote <senepote@gmail.com>
2025-01-17 17:55:39 -0600

    added more slides and styling fixes

--- js/main.js
+++ js/main.js
@@ -78,23 +78,45 @@ $(() => {
   
 })
  
-let slideIndex = 1;
-showSlides(slideIndex);function plusSlide(n) {
-  showSlides(slideIndex += n);
-}function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}function prevSlide() {
-  plusSlide(-1);
-}function nextSlide() {
-  plusSlide(1);
+// let slideIndex = 1;
+// showSlides(slideIndex);function plusSlide(n) {
+//   showSlides(slideIndex += n);
+// }function showSlides(n) {
+//   let i;
+//   const slides = document.getElementsByClassName("slide");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slides[slideIndex-1].style.display = "block";
+// }function prevSlide() {
+//   plusSlide(-1);
+// }function nextSlide() {
+//   plusSlide(1);
+// }
+
+var slideIndex = [1,1,1];
+var slideId = ["slide", "slide2", "slide3"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
 
 function openPortfolioTab(sectionName) {
   var i;
